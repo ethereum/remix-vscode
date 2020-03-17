47396392bd135e77c193d3c67444569c3e12d15d
0mkar <0mkar@protonmail.com>
2020-03-18 01:00:42 +0530

    list plugins

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,9 @@ import * as vscode from 'vscode';
 import { DepNodeProvider } from './nodeDependencies';
 
 export function activate(context: vscode.ExtensionContext) {
-
 	// Samples of `window.registerTreeDataProvider`
 	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+	// vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
 }
--- src/nodeDependencies.ts
+++ src/nodeDependencies.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import { Uri} from 'vscode';
+import axios from 'axios';
 
 export class DepNodeProvider implements vscode.TreeDataProvider<PluginInterface> {
 
@@ -19,65 +19,30 @@ export class DepNodeProvider implements vscode.TreeDataProvider<PluginInterface>
 	}
 
 	getChildren(element?: PluginInterface): Thenable<PluginInterface[]> {
-		if (!this.workspaceRoot) {
-			vscode.window.showInformationMessage('No dependency in empty workspace');
-			return Promise.resolve([]);
-		}
-
-		if (element) {
-			return Promise.resolve(this.getDepsInPackageJson(path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')));
-		} else {
-			const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
-			if (this.pathExists(packageJsonPath)) {
-				return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
-			} else {
-				vscode.window.showInformationMessage('Workspace has no package.json');
-				return Promise.resolve([]);
-			}
-		}
-
+		return this.getRmxPlugins()
+			.then(children => {
+				return Promise.resolve(children);
+			})
 	}
 
-	/**
-	 * Given the path to package.json, read all its dependencies and devDependencies.
-	 */
-	private getDepsInPackageJson(packageJsonPath: string): PluginInterface[] {
-		if (this.pathExists(packageJsonPath)) {
-			const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-
-			const toDep = (moduleName: string, version: string): PluginInterface => {
-				if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
-					return new PluginInterface(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed);
-				} else {
-					return new PluginInterface(moduleName, version, vscode.TreeItemCollapsibleState.None, {
-						command: 'extension.openPackageOnNpm',
-						title: '',
-						arguments: [moduleName]
-					});
-				}
-			};
-			
-			const deps = packageJson.remixPlugins
-				? Object.keys(packageJson.remixPlugins).map(dep => toDep(dep, packageJson.dependencies[dep]))
-				: [];
-			// TODO: may be we could also have dev plugins for plugin developers
-			// const devDeps = packageJson.devDependencies
-			// 	? Object.keys(packageJson.devDependencies).map(dep => toDep(dep, packageJson.devDependencies[dep]))
-			// 	: [];
-			// return deps.concat(devDeps);
-			return deps
-		} else {
-			return [];
-		}
-	}
-
-	private pathExists(p: string): boolean {
+	private async getRmxPlugins(): Promise<PluginInterface[]> {
+		const toPlugin = (pluginName: string, version: string, icon: string): PluginInterface => {
+			return new PluginInterface(pluginName, version, vscode.TreeItemCollapsibleState.None, {
+				command: '',
+				title: pluginName,
+				arguments: [pluginName]
+			}, Uri.parse(icon));
+		};
 		try {
-			fs.accessSync(p);
-		} catch (err) {
-			return false;
+			// fetch plugins from https://raw.githubusercontent.com/ethereum/remix-plugins-directory/master/build/metadata.json
+			const resp = await axios.get('https://raw.githubusercontent.com/ethereum/remix-plugins-directory/master/build/metadata.json');
+			const { data } = resp;
+			console.log(data);
+			const plugins = data ? data.map(plugin => toPlugin(plugin.displayName, plugin.version, plugin.icon)) : [];
+			return Promise.resolve(plugins);
+		} catch(error) {
+			throw error;
 		}
-		return true;
 	}
 }
 
@@ -86,10 +51,10 @@ export class PluginInterface extends vscode.TreeItem {
 		public readonly label: string,
 		private version: string,
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-		public readonly command?: vscode.Command
-	) {
-		super(label, collapsibleState);
-	}
+		public readonly command?: vscode.Command,
+		public readonly iconURI?: Uri) {
+			super(label, collapsibleState);
+		}
 	get tooltip(): string {
 		return `${this.label}-${this.version}`;
 	}
@@ -97,8 +62,8 @@ export class PluginInterface extends vscode.TreeItem {
 		return this.version;
 	}
 	iconPath = {
-		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+		light: this.iconURI,
+		dark: this.iconURI
 	};
 	contextValue = 'plugin';
 }

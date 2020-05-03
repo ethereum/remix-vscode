75fa441551eec2ba04a941b8c9884454007653db
0mkar <0mkar@protonmail.com>
2020-05-03 23:42:06 +0530

    register plugin

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,22 @@
 
 import * as vscode from 'vscode';
 import { Uri } from 'vscode';
-import { PluginManager } from '@remixproject/engine';
+import { PluginManager, Engine } from '@remixproject/engine';
 
 import { RmxPluginsProvider } from './rmxPlugins';
 
-const manager = new PluginManager({});
+const manager = new PluginManager();
 
 export function activate(context: vscode.ExtensionContext) {
 	const rmxPluginsProvider = new RmxPluginsProvider(vscode.workspace.rootPath);
 	vscode.window.registerTreeDataProvider('rmxPlugins', rmxPluginsProvider);
 	// vscode.commands.registerCommand('extension.activateRmxPlugin', pluginName => RemixPanel.createOrShow(pluginName));
 	vscode.commands.registerCommand('extension.activateRmxPlugin', pluginName => {
-		manager.activatePlugin([pluginName]);
+		console.log("activating plugin ", pluginName);
+		const engine = new Engine(manager);
+		engine.onload(() => {
+			console.log("Engine loaded");
+			engine.register(pluginName);
+		});
 	});
 }

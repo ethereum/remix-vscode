5508135df0dc68bb459a5c49e823f5600eda8dcf
filip mertens <filip.mertens@ethereum.org>
2021-04-29 10:16:19 +0200

    payload optional error checking

--- src/plugins/ext_api_plugin.ts
+++ src/plugins/ext_api_plugin.ts
@@ -1,24 +1,31 @@
-import { extensions } from "vscode";
+import { extensions, window } from "vscode";
 import { CommandPlugin } from "@remixproject/engine-vscode";
 
 const profile = {
-  name: 'vscodeExtAPI',
-  displayName: 'Extension API connector',
-  description: 'Connects VScode Extension API with remix plugins.',
-  kind: 'connector',
+  name: "vscodeExtAPI",
+  displayName: "Extension API connector",
+  description: "Connects VScode Extension API with remix plugins.",
+  kind: "connector",
   permission: true,
-  documentation: 'https://remix-ide.readthedocs.io/en/latest/solidity_editor.html',
-  version: '0.0.1',
-  methods: ['executeCommand']
+  documentation:
+    "",
+  version: "0.0.1",
+  methods: ["executeCommand"],
 };
 export class ExtAPIPlugin extends CommandPlugin {
-	constructor() {
-		super(profile);
-	}
-	async executeCommand(extName: string, cmdName: string, params: Array<any>) {
-		const ext = extensions.getExtension(extName);
-    await ext.activate();
-    const extAPI = ext.exports;
-    extAPI[cmdName](...params);
-	}
+  constructor() {
+    super(profile);
+  }
+  async executeCommand(extName: string, cmdName: string, payload?: any[]) {
+    try {
+      const ext = extensions.getExtension(extName);
+      await ext.activate();
+      const extAPI = ext.exports;
+      extAPI[cmdName](...payload || []);
+    } catch (e) {
+      // extension can not be activated or any other error
+      console.error(e)
+      window.showErrorMessage(`${extName} extension is not installed or disabled.`)
+    }
+  }
 }
